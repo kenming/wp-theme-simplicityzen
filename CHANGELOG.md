26717b5728711cc83027beccfa10c8cf9eaef442
Kenming Wang <kenming.wang@gmail.com>
2018-03-06 15:39:07 +0800

    fix index.php to suite Bootstrap 4 grid layout

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+- ** Release 0.6.0 02/27/2018 **
+  - Integrated the _S plus Bootstrap & Font Awesome
+
+- ** Release 0.6.2 03/06/2018 **
+  - add CHANGELOG.md
+  - fix index.php to suite Bootstrap 4 grid layout
--- index.php
+++ index.php
@@ -12,10 +12,12 @@
  * @package SimplicityZen
  */
 
-get_header(); ?>
+?>
 
-	<div id="primary" class="content-area col-md-8">
-		<main id="main" class="site-main">
+<?php get_header(); ?>
+
+<div id="primary" class="content-area col-md-8">
+	<main id="main" class="site-main">
 
 		<?php
 		if ( have_posts() ) :
@@ -32,10 +34,10 @@ get_header(); ?>
 			while ( have_posts() ) : the_post();
 
 				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
+					* Include the Post-Format-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					*/
 				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
@@ -48,10 +50,11 @@ get_header(); ?>
 
 		endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <div class="col-md-4">
 	<?php get_sidebar(); ?>
 </div>
+
 <?php get_footer();
